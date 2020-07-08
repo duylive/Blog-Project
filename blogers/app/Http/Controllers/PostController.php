8056b30144b200ce08fe873532b8eb2d6211b69f
duylive <duylive92@gmail.com>
2020-07-07 22:45:32 -0700

    update

--- blogers/app/Http/Controllers/PostController.php
+++ blogers/app/Http/Controllers/PostController.php
@@ -13,7 +13,7 @@ class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::paginate(2);
+        $posts = Post::latest()->paginate(2);
         return view('posts.list', compact('posts'));
     }
 
